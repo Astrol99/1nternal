0c7e056c622b119de7e92237fb805e143e4ba13a
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-04-13 11:28:47 -0500

    Added thread and console creation

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,18 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+DWORD WINAPI MainThread(HMODULE hModule)
+{
+    // Create console from thread
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+
+    std::cout << "1nternal" << std::endl;
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,6 +22,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr));
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
