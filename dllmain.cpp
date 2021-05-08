fc3010d55918bb7f6dff74c5ecaa16f83a21524d
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-05-08 01:14:31 -0500

    Added aimbot calculations and godmode health

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,28 +1,29 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "structs.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
-#include <cmath>
 
-Vec3* normalize(Vec3* src, Vec3* dst)
+Vec3 normalize(Vec3 src, Vec3 dst)
 {
     // Normalize src to the origin (0,0,0) relative to dst
-    Vec3 normalized = Vec3{
-        dst->x - src->x,
-        dst->y - src->y,
-        dst->z - dst->z
+     Vec3 normalized = Vec3{
+        dst.x - src.x,
+        dst.y - src.y,
+        dst.z - src.z
     };
 
-    return &normalized;
+    return normalized;
 }
 
 // 3D Euclidian distance forumula function
-float dist(Vec3* src, Vec3* dst)
+float dist(Vec3 src, Vec3 dst)
 {
-    Vec3* normalized = normalize(src, dst);
-    return sqrtf(powf(normalized->x, 2) + powf(normalized->y, 2) + powf(normalized->z, 2));
+    Vec3 normalized = normalize(src, dst);
+    return sqrtf(powf(normalized.x, 2) + powf(normalized.y, 2) + powf(normalized.z, 2));
 }
 
 Player* getClosestPlayer(Game* game)
@@ -35,15 +36,17 @@ Player* getClosestPlayer(Game* game)
 
         // If the closest player is null (first loop), assign closest as first
         // player in loop then skip to second loop to continue normal operations
-        // BUGGGGGGGGG RIGHT HEREEEEEEEEEE continue AFTERRRRRR THISSSSSSSSS
-        if (!closest)
+        if (i == 1)
+        {
             closest = player;
+            continue;
+        }
 
         // If player team is not on the same team as localplayer
         if (player->m_Team != game->m_LocalPlayer->m_Team)
         {
             // Get lowest dist between previous closest player and current indexed player
-            if (dist(&game->m_LocalPlayer->m_HeadPos, &player->m_HeadPos) < dist(&game->m_LocalPlayer->m_HeadPos, &closest->m_HeadPos))
+            if (dist(game->m_LocalPlayer->m_HeadPos, player->m_HeadPos) < dist(game->m_LocalPlayer->m_HeadPos, closest->m_HeadPos))
                 closest = player;
         }
     }
@@ -73,6 +76,17 @@ DWORD WINAPI MainThread(HMODULE hModule)
         game = game->GetInstance();
 
         Player* closestPlayer = getClosestPlayer(game);
+
+        // Misc
+        game->m_LocalPlayer->m_Health = 999;
+
+        // Main aimbot calculation stuff
+        Vec3 normalized = normalize(game->m_LocalPlayer->m_HeadPos, closestPlayer->m_HeadPos);
+
+        game->m_LocalPlayer->m_Yaw = (atan2f(normalized.y, normalized.x) * (180/ M_PI)) + 90;
+        game->m_LocalPlayer->m_Pitch = (atan2f(normalized.z, dist(game->m_LocalPlayer->m_HeadPos, closestPlayer->m_HeadPos)) * (180 / M_PI));
+
+        std::cout << normalized.z << std::endl;
 
         Sleep(5); // Preserve some resources
     }
