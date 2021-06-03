439a9c5e93858f7436d0a5ba3d14c8c64be4d40b
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-06-03 18:09:35 -0500

    Added screen setting related offset structs

--- structs.h
+++ structs.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#define OFFSET_SCREENSETTINGS      0x510C94
 #define OFFSET_GAME                0x50F4E8
 #define VIEW_MATRIX_ADDR		   0x501AE8
 
@@ -180,3 +181,25 @@ public:
     }
 
 };//Size=0x0030
+
+class ScreenSettings
+{
+public:
+	__int32 m_Width; //0x0000 
+	__int32 m_Height; //0x0004 
+	char _0x0008[8];
+	__int32 m_GraphicDetail; //0x0010 VIDEO: INSANE - MEDIUM = 8, LOWER = 0 
+	__int32 m_AntiAlias; //0x0014  VIDEO: INSANE = 8, HIGH = 4, LOWER = 0
+	__int32 m_VSync; //0x0018 
+	BYTE m_Fullscreen; //0x001C 
+	char _0x001D[23];
+	__int32 m_Gamma; //0x0034 
+	__int32 m_MaxFPS; //0x0038 
+	char _0x003C[12];
+	__int32 m_GameSpeed; //0x0048   100 is normal
+	char _0x004C[52];
+
+	static ScreenSettings* GetInstance() {
+		return (ScreenSettings*)(OFFSET_SCREENSETTINGS);
+	}
+};//Size=0x0080
